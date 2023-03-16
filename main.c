dff8d70a82d2506347462edc25f77f03f287a30a
prasdudrambles <124054712+prasdudrambles@users.noreply.github.com>
2023-03-16 21:04:13 +0530

    Update main.c
    don't know if the code actually works as i don't own a mq-135 sensor, but theoretically should work.

--- main.c
+++ main.c
@@ -1,16 +1,17 @@
 #include <ESP8266WiFi.h>
 #include <ThingSpeak.h>
 
-// WiFi settings
+//Access point credentials
+//point to a different file for security reasons
 const char* ssid = "your_SSID_here";
 const char* password = "your_PASSWORD_here";
 
-// ThingSpeak settings
+//initializing thingspeak API
 unsigned long channelID = your_channel_ID_here;
 const char* writeAPIKey = "your_write_API_key_here";
 const char* server = "api.thingspeak.com";
 
-// Gas sensor settings
+// initial MQ-135 settings
 int sensorValue = 0;
 int airQualityIndex = 0;
 int samplingTime = 280;
@@ -20,33 +21,29 @@ int pin = A0;
 void setup() {
   Serial.begin(115200);
 
-  // Connect to WiFi
+  //connect to AP
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi...");
   }
-
-  // Initialize ThingSpeak
+  //thingspeak api
   ThingSpeak.begin(client);
 }
 
 void loop() {
-  // Read analog value from gas sensor
+  //Read analog value from gas sensor
   sensorValue = analogRead(pin);
   
-  // Calculate air quality index
+  // Calculation of AQI
   float Rs = 1023.0 / (float)sensorValue - 1.0;
   float ratio = Rs / (float)(samplingTime * 9.8);
   airQualityIndex = 0.25 * ratio * 10000 / (5.0 - ratio);
 
-  // Print air quality index
+  //AQI
   Serial.print("Air Quality Index: ");
   Serial.println(airQualityIndex);
-
-  // Update ThingSpeak channel
+  //sync with thingspeak
   ThingSpeak.writeField(channelID, 1, airQualityIndex, writeAPIKey);
-
-  // Wait for 15 seconds before taking another reading
   delay(15000);
 }
